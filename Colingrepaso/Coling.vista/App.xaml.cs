84a6b71b999490d7910eb8c1d3ff1dfb67e227ee
jhorel123 <jhorelrolandoa@gmail.com>
2024-03-21 13:40:52 -0400

    Proyecto base

---
+++ Colingrepaso/Coling.vista/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Coling.vista
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Colingrepaso/Coling.vista/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Coling.vista
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
